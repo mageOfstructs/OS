951c99ba2645043e1f895a5fb3fc174c2bf5e015
mageOfStructs <jason.puschnig@gmx.at>
2025-07-19 00:44:26 +0200

    add cross compiler infra

--- boot.asm
+++ boot.asm
@@ -1,53 +1,90 @@
-[org 0x7c00]
+[org 0x7c00]                        
+KERNEL_LOCATION equ 0x1000
+                                    
 
-hello:
-  db "Hello World", 0
-furry:
-  db "UWU", 0
+mov [BOOT_DISK], dl                 
 
+                                    
+xor ax, ax                          
+mov es, ax
+mov ds, ax
 mov bp, 0x8000
 mov sp, bp
 
-mov ax, 160
-call prtint
+mov bx, KERNEL_LOCATION
+mov dh, 2
+
+mov ah, 0x02
+mov al, dh ; number of sectors to read, important
+mov ch, 0x00
+mov dh, 0x00
+mov cl, 0x02
+mov dl, [BOOT_DISK]
+int 0x13                ; no error management, do your homework!
+
+                                    
+mov ah, 0x0
+mov al, 0x3
+int 0x10                ; text mode
+
+
+CODE_SEG equ GDT_code - GDT_start
+DATA_SEG equ GDT_data - GDT_start
+
+cli
+lgdt [GDT_descriptor]
+mov eax, cr0
+or eax, 1
+mov cr0, eax
+jmp CODE_SEG:start_protected_mode
 
 jmp $
+                                    
+BOOT_DISK: db 0
 
-; prints a NUL-terminated string
-; bx: pointer to str
-strprt:
-  mov ah, 0x0e
-loop:
-  mov al, [bx]
-  cmp al, 0
-  je endloop
-  int 0x10
-  add bx,2
-  jmp loop
-endloop:
-  ret
+GDT_start:
+    GDT_null:
+        dd 0x0
+        dd 0x0
 
-prtint:
-  mov dl, 10
-  mov dh, 48
-  mov ch, 0
-  push 0
-prtint_loop:
-  div dl
-  mov cl, ah
-  or cl, dh
-  push cx
-  
-  ; nothing to divide anymore
-  cmp al, 0
-  je prtint_endloop
+    GDT_code:
+        dw 0xffff
+        dw 0x0
+        db 0x0
+        db 0b10011010
+        db 0b11001111
+        db 0x0
 
-  mov ah, 0
-  jmp prtint_loop
-prtint_endloop:
-  mov bx, sp
-  call strprt
-  ret
+    GDT_data:
+        dw 0xffff
+        dw 0x0
+        db 0x0
+        db 0b10010010
+        db 0b11001111
+        db 0x0
 
-times 510-($-$$) db 0
-db 0x55, 0xaa
+GDT_end:
+
+GDT_descriptor:
+    dw GDT_end - GDT_start - 1
+    dd GDT_start
+
+
+[bits 32]
+start_protected_mode:
+    mov ax, DATA_SEG
+	mov ds, ax
+	mov ss, ax
+	mov es, ax
+	mov fs, ax
+	mov gs, ax
+	
+	mov ebp, 0x90000		; 32 bit stack base pointer
+	mov esp, ebp
+
+    jmp KERNEL_LOCATION
+
+                                     
+ 
+times 510-($-$$) db 0              
+dw 0xaa55
