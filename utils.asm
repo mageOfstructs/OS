b9050b8113d368a2ceb01cb523bbeeaf55064454
mageOfStructs <jason.puschnig@gmx.at>
2025-08-22 23:35:08 +0200

    lemme cook

--- utils.asm
+++ utils.asm
@@ -1,11 +1,13 @@
-global strprt
+global prtint
+extern write_str
 
 section .text
 
 [bits 32]
 strprt:
-  mov ecx, 0xb8000
-  mov edx, [esp+4] ; get the pointer we received as parameter
+  ; mov edx, [esp+4] ; get the pointer we received as parameter
+  ; mov ecx, 0xb8000
+  ; mov ecx, [esp+8]
   mov ah, 0x0f ; set color to white
 strprt_loop:
   mov al, [edx]
@@ -15,9 +17,34 @@ strprt_loop:
   
   mov [ecx], ax
 
-  inc edx
+  add edx, 2 ; stupid 16-bit stack alignment...
   add ecx, 2
   
   jmp strprt_loop
 strprt_loopend:
+  ret
+
+[bits 32]
+prtint:
+  mov eax, [esp+4]
+  mov edx, [esp+8] ; base
+  mov ecx, [esp+12] ; address in video memory
+  mov bh, 0
+  push 0
+prtint_loop:
+  div edx
+  mov bl, ah ; move out of ah, since that is needed in the next iteration
+  or bl, 48 ; turn modulo into printable character
+
+  push bx
+ 
+  ; nothing to divide anymore
+  cmp al, 0
+  je prtint_endloop
+
+  mov ah, 0
+  jmp prtint_loop
+prtint_endloop:
+  mov edx, esp
+  call strprt
   ret
