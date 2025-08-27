5eb206691aa177bdc80add76feb504e7ba0d4c2e
mageOfStructs <mageOfStructs@proton.me>
2025-08-27 10:03:01 +0200

    refactor out/in wrappers

---
+++ io.c
@@ -0,0 +1,18 @@
+#include <stdint.h>
+inline void outb(uint16_t dev, char val) {
+  asm volatile("out dx, al" : : [dev] "d"(dev), [val] "a"(val));
+}
+
+inline char inb(uint16_t dev) {
+  char ret;
+  asm volatile("mov dx, %[dev]\n\t"
+               "in %0, dx\n\t"
+               // "mov %0, al"
+               : "=rm"(ret)
+               : [dev] "r"(dev));
+  return ret;
+}
+
+inline void io_wait(void) {
+    outb(0x80, 0);
+}
---
+++ io.h
@@ -0,0 +1,3 @@
+void outb(u16 dev, char val);
+char inb(u16 dev);
+void io_wait(void);
