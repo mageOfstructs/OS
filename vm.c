b3c7c3427e20394fa34ff9d46523a2d7b96f6d71
mageOfStructs <mageOfStructs@proton.me>
2025-08-29 15:34:50 +0200

    add primitive allocator

--- vm.c
+++ vm.c
@@ -1,18 +1,35 @@
 #include "vm.h"
 #include "printf.h"
 
-uint32_t page_dir[1024] __attribute__((aligned(4096)));
-uint32_t kernel_pt[1024] __attribute__((aligned(4096)));
+static uint32_t page_dir[1024] __attribute__((aligned(4096)));
+static uint32_t kernel_pt[1024] __attribute__((aligned(4096)));
 
-void fill_pde(pde_t *p, uint32_t addr, bool rw, bool us) {
+// static uint32_t pt_space_pt[1024] __attribute__((aligned(4096)));
+static uint32_t *pt_space_pt = (uint32_t*)(0x0);
+static uint32_t *pt_space = (uint32_t*)(0x400000); // note: need to increase it by 1024 to access the next table
+static uint8_t free_pts_bitmap[128] = { 255 };
+
+void fill_pde(pde_t *p, uint32_t addr, bool write_allowed, bool available_to_userspace) {
   p->flags = 1; // we'll do swap later
-  if (rw)
+  if (write_allowed)
     p->flags |= 2;
-  if (us)
+  if (available_to_userspace)
     p->flags |= 4;
+  p->avladdr = (uint8_t)(addr >> 8);
+  p->haddr = (uint16_t)(addr >> 16);
+}
+
+void fill_pte(pte_t *p, uint32_t addr, bool write_allowed, bool available_to_userspace, bool global) {
+  p->flags = 1;
+  if (write_allowed)
+    p->flags |= 2;
+  if (available_to_userspace)
+    p->flags |= 4;
+  p->g = global & 1; // make sure we don't overflow the bit field
   p->avl = 0;
-  p->laddr = (uint16_t)(addr >> 12);
-  p->haddr = (uint8_t)(addr >> 28);
+  p->laddr = (uint8_t)(addr >> 12) & 0x0F;
+  p->haddr = (uint16_t)(addr >> 16);
+  // printf("PTE size: %d\n", sizeof(pte_t));
 }
 
 void *get_physaddr(void *virtualaddr) {
@@ -21,25 +38,74 @@ void *get_physaddr(void *virtualaddr) {
 
   unsigned long *pd = page_dir;
   // Here you need to check whether the PD entry is present.
-  printf("pdindex: %d\n", pdindex);
-  printf("ptindex: %d\n", ptindex);
 
-  unsigned long *pt = pd[pdindex] & ~0xFFF;
-  printf("%p == %p?\n", pt, kernel_pt);
+  unsigned long *pt = (uint32_t *)(pd[pdindex] & ~0xFFF);
+  printf("PT: %p ", pt);
   // Here you need to check whether the PT entry is present.
 
-  printf("PTE: %p\n", (pt[ptindex]));
+  printf("PTE: %p ", (pt[ptindex]));
   printf("Page: %p\n", (pt[ptindex] & ~0xFFF));
 
   return (void *)((pt[ptindex] & ~0xFFF) +
                   ((unsigned long)virtualaddr & 0xFFF));
 }
 
+static inline bool is_pde_present(uint32_t i) {
+  return page_dir[i] & 1;
+}
+
+static inline bool is_pte_present(uint32_t *pt, uint32_t i) {
+  return pt[i] & 1;
+}
+
+uint32_t *alloc_pt() {
+  for (int i = 0; i < 1024; i++) {
+    if ((free_pts_bitmap[i / 8] >> (i % 8)) & 1) {
+      free_pts_bitmap[i / 8] |= 1 << (i % 8);
+      for (int j = 0; j < 1024; j++) {
+        pt_space[i*1024+j] = BLANK_PTE;
+      }
+      return &pt_space[i*1024];
+    }
+  }
+  return NULL;
+}
+
+/**
+ * helper method for vm mapping
+ * will fail if address is already mapped, therefore it doesn't do a TLB flush
+**/
+int vm_map(uint32_t vaddr_start, uint32_t paddr_start, uint32_t len) {
+  uint32_t pd_i = vaddr_start >> 22;
+  uint32_t pt_i = vaddr_start >> 12 & 0x03FF;
+  if (is_pde_present(pd_i)) {
+    uint32_t *pt = (uint32_t*) page_dir[pd_i];
+    if (is_pte_present(pt, pt_i)) // address is already mapped
+      return 1;
+    fill_pte((pte_t*)&pt[pt_i], paddr_start, true, false, false);
+  } else {
+    uint32_t *pt = alloc_pt();
+    if (!pt) return 2; // ran out of pt_space
+    fill_pde((pde_t*)&page_dir[pd_i], (uint32_t)pt, true, false);
+    printf("vm_map: %p == %p? \n", (uint32_t)pt, page_dir[pd_i]);
+    fill_pte((pte_t*)&pt[pt_i], paddr_start, true, false, false);
+  }
+  return 0; // success
+} 
+
 void enable_paging(void) {
   printf("page_dir addr is %p\n", page_dir);
-  printf("physaddr of 0x1000 is: %p", get_physaddr((void *)0x1000));
-  printf("physaddr of 0x100f is: %p", get_physaddr((void *)0x100F));
-  printf("physaddr of 0x5000 is: %p", get_physaddr((void *)0x5000));
+  printf("physaddr of 0x1000 is: %p\n", get_physaddr((void *)0x1000));
+  printf("physaddr of 0x100f is: %p\n", get_physaddr((void *)0x100F));
+  printf("physaddr of 0x4000 is: %p\n", get_physaddr((void *)0x4000));
+  printf("physaddr of 0x4800 is: %p\n", get_physaddr((void *)0x4800));
+  printf("physaddr of 0x1410 is: %p\n", get_physaddr((void *)0x1410));
+
+  printf("physaddr of 0x400000 is: %p\n", get_physaddr((void *)0x400000));
+  int map_status = vm_map(0x12345678, 0x00800000, 1);
+  printf("vm_map status: %d\n", map_status);
+  printf("physaddr of 0x12345678 is: %p\n", get_physaddr((void *)0x12345678));
+  // for (;;) asm("cli; hlt");
   asm volatile("mov eax, %0\n\t"
                "mov cr3, eax\n\t"
                "mov eax, cr0\n\t"
@@ -47,21 +113,7 @@ void enable_paging(void) {
                "mov cr0, eax\n\t" ::"g"(
                    page_dir)); // okay so I have no idea what/why just happened
                                // but I added the g here and now it works?????
-  // for (;;) asm("hlt");
 }
-
-// void setup_vm(void) {
-//   for (int i = 0; i < 1024; i++)
-//     page_dir[i] = BLANK_PDE;
-//   // map kernel to exactly where it currently is
-//   for (int i = 0; i < 1024; i++) {
-//     kernel_pt[i] = (i * 0x1000) | 3;
-//   }
-//   page_dir[1] = ((unsigned int)kernel_pt) | 3;
-//
-//   enable_paging();
-//   for (;;) asm("hlt");
-// }
 
 void setup_vm(void) {
   // set each entry to not present
@@ -74,16 +126,17 @@ void setup_vm(void) {
     page_dir[i] = 0x00000002;
   }
 
-  // holds the physical address where we want to start mapping these pages to.
-  // in this case, we want to map these pages to the very beginning of memory.
-
   // we will fill all 1024 entries in the table, mapping 4 megabytes
   for (i = 0; i < 1024; i++) {
     // As the address is page aligned, it will always leave 12 bits zeroed.
     // Those bits are used by the attributes ;)
     kernel_pt[i] =
         (i * 0x1000) | 3; // attributes: supervisor level, read/write, present.
+
+    pt_space_pt[i] = (uint32_t)(((i * 0x1000 + (uint32_t)pt_space)) | 3);
   }
-  page_dir[0] = ((unsigned long)kernel_pt) | 3;
+  fill_pde((pde_t *)page_dir, (unsigned long)kernel_pt, true, false);
+  fill_pde((pde_t *)(&page_dir[1]), (unsigned long)pt_space_pt, true, false);
+
   enable_paging();
 }
--- vm.h
+++ vm.h
@@ -1,5 +1,11 @@
+#ifndef VM_H
+
+#define VM_H
+
 #include <stdint.h>
 #include <stdbool.h>
+
+#define NULL 0
 
 void setup_vm(void);
 
@@ -16,12 +22,12 @@ void setup_vm(void);
 */
 typedef struct pde {
   uint8_t flags;
-  uint8_t avl: 4;
-  uint16_t laddr; // bits 12-27 of addr
-  uint8_t haddr : 4; // bits 28-31 of addr
+  uint8_t avladdr; // lower 4 bits are avl, higher ones are bits 12-15 of addr
+  uint16_t haddr; // bits 16-31 of addr
 } __attribute__((packed)) pde_t;
 
 #define BLANK_PDE 0x00000002
+#define BLANK_PTE BLANK_PDE
 
 /*
  * flags:
@@ -38,6 +44,8 @@ typedef struct pte {
   uint8_t flags;
   uint8_t g : 1;
   uint8_t avl: 3;
-  uint16_t laddr; // bits 12-27 of addr
-  uint8_t haddr : 4; // bits 28-31 of addr
+  uint8_t laddr : 4; // bits 12-15 of addr
+  uint16_t haddr; // bits 16-31 of addr
 } __attribute__((packed)) pte_t;
+
+#endif // !VM_H
