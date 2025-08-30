a1ca897e16c886dc1e35114bc8651537847c79e1
mageOfStructs <jason.puschnig@gmx.at>
2025-08-30 17:44:47 +0200

    relocate GDT so we don't overwrite it

--- kernel.c
+++ kernel.c
@@ -1,10 +1,31 @@
 #include "idt.h"
-#include "vm.h"
 #include "pic.h"
 #include "printf.h"
+#include "vm.h"
 #include <stdint.h>
 
+static uint64_t GDT[3];
+static uint8_t GDTR[6];
+
 int main() {
+  // relocate the GDT as setup_vm would otherwise overwrite it with a page table
+  // NOTE: This is not a permanent solution! It could be that the kernel grows
+  // so big it's code would overlap with the bootsector and overwrite it when it
+  // is loaded into memory.
+  uint32_t gdt_addr = (uint32_t)GDT;
+  for (int i = 0; i < 4; i++) {
+    GDTR[i + 2] = (gdt_addr >> i * 8) & 0xFF;
+  }
+  asm("mov ecx, 8*3\n\t"
+      "mov esi, 0x7c42\n\t"
+      "mov edi, %0\n\t"
+      "rep; movsb\n\t"
+      "mov ecx, 2\n\t"
+      "mov esi, 0x7c5a\n\t"
+      "mov edi, %1\n\t"
+      "rep; movsb\n\t"
+      "lgdt %2" ::"g"(GDT),
+      "g"(GDTR), "m"(GDTR));
   idt_init();
   PIC_remap(0x20, 0x28);
   setup_vm();
@@ -15,8 +36,8 @@ int main() {
   int asdf = 43;
   // printf("%c", *((char *)0x99999999));
   // printf("%s %s %s", test, "asdf", "fdas");
-  printf("test\n");
-  printf("test2\n");
+  // printf("test\n");
+  // printf("test2\n");
   // int tmp = 1 / 0;
   // printf("test3\n");
   // asm volatile("int 0x00");
