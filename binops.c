2b6e850c8c63e7636f19ff339e86dc17259b546e
mageOfStructs <jason.puschnig@gmx.at>
2025-08-30 17:44:30 +0200

    add utils for bit manipulation

---
+++ binops.c
@@ -0,0 +1,5 @@
+#include "binops.h"
+
+bool get_bit(uint8_t *bits, uint32_t i) { return (bits[i / 8] >> (i % 8)) & 1; }
+void set_bit(uint8_t *bits, uint32_t i) { bits[i / 8] |= 1 << (i % 8); }
+void clear_bit(uint8_t *bits, uint32_t i) { bits[i / 8] &= ~(1 << (i % 8)); }
