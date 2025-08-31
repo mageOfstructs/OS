60a04d3452a4878e572de00ed7be5664222d525c
mageOfStructs <jason.puschnig@gmx.at>
2025-08-31 21:04:47 +0200

    inline binops

--- binops.c
+++ binops.c
@@ -1,5 +1,9 @@
 #include "binops.h"
 
-bool get_bit(uint8_t *bits, uint32_t i) { return (bits[i / 8] >> (i % 8)) & 1; }
-void set_bit(uint8_t *bits, uint32_t i) { bits[i / 8] |= 1 << (i % 8); }
-void clear_bit(uint8_t *bits, uint32_t i) { bits[i / 8] &= ~(1 << (i % 8)); }
+inline bool get_bit(uint8_t *bits, uint32_t i) {
+  return (bits[i / 8] >> (i % 8)) & 1;
+}
+inline void set_bit(uint8_t *bits, uint32_t i) { bits[i / 8] |= 1 << (i % 8); }
+inline void clear_bit(uint8_t *bits, uint32_t i) {
+  bits[i / 8] &= ~(1 << (i % 8));
+}
