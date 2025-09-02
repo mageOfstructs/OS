2982d57943fbeebb78a163875f15cb2c760c8a63
mageOfStructs <mageOfStructs@proton.me>
2025-09-02 13:08:46 +0100

    We've made major strides since then...major strides...

--- fs/ext2.c
+++ fs/ext2.c
@@ -1,5 +1,9 @@
 #include "ext2.h"
 
+uint16_t sp_buf[1024];
+uint16_t inode_sz;
+bool dir_have_ti = false;
+
 static inline bool is_ext2_fs(superblock_t *sp) {
   return sp->sig == EXT2_SIG;
 }
@@ -8,6 +12,12 @@ int is_fs_not_ok(superblock_t *sp) {
   if (!is_ext2_fs(sp)) return 1;
   if (sp->fs_state == 2) return 1;
   if (sp->fs_state == 2 && sp->err_handling_method != 1) return 1;
+  if (sp->major_version >= 1) {
+    printf("%p", sp->required_features);
+    if (sp->required_features & FEAT_REQ_COMP) return 2;
+    if (sp->required_features & FEAT_REQ_NEEDS_JOURNAL_REPLAY) return 4;
+    if (sp->required_features & FEAT_REQ_JOURNAL_DEV) return 5;
+  }
   return 0;
 }
 
@@ -21,11 +31,63 @@ uint32_t get_block_size(superblock_t *sp) {
   return 1024 << sp->log2_bs;
 }
 
-uint32_t block_to_phys_addr(superblock_t *sp, uint32_t block_addr) {
+inline uint32_t block_to_phys_addr(superblock_t *sp, uint32_t block_addr) {
   return block_addr * get_block_size(sp);
 }
 
-uint16_t sp_buf[1024];
+uint32_t get_inode_sz(superblock_t *sp) {
+  if (sp->major_version >= 1) return sp->inode_sz;
+  return 128;
+}
+
+int read_block(superblock_t *sp, uint32_t bg, uint32_t block, uint16_t *ret) {
+  uint32_t real_addr = bg * sp->blocks_per_bg * get_block_size(sp) + block * get_block_size(sp);
+  return read(true, real_addr, get_block_size(sp), ret);
+}
+
+uint32_t read_inode(superblock_t *sp, bg_desc_t bgdt[], uint32_t inode_addr) {
+  uint32_t block_group = (inode_addr - 1) / sp->inodes_per_bg;
+  uint32_t bg_inodet_index = (inode_addr - 1) % sp->inodes_per_bg;
+  uint32_t containing_block = (bg_inodet_index * inode_sz) / get_block_size(sp);
+
+  uint16_t *inode_buf = kalloc(get_block_size(sp));
+  uint32_t inode_start = bg_inodet_index * inode_sz;
+  // printf("inode table lives at %p\n", bgdt[block_group].inode_table_addr);
+  // printf("Inode table index: %d\n", bg_inodet_index);
+  // printf("Reading block %d from block group %d\n", bgdt[block_group].inode_table_addr + containing_block, block_group);
+  KASSERT(read_block(sp, block_group, bgdt[block_group].inode_table_addr + bg_inodet_index, inode_buf) == 0);
+  // for (int i = 0; i < get_block_size(sp); i++) {
+  //   if (inode_buf[i])
+  //     printf("%p ", inode_buf[i]);
+  // }
+  inode_t *i = &inode_buf[inode_start];
+  printf("Inode Type: %p\n", i->typeperm);
+  printf("Inode Size: %d\n", i->lsize);
+  int cur_ptr = 0;
+  uint16_t *dir_entry_buf = kalloc(get_block_size(sp));
+  KASSERT(read_block(sp, 0, i->dptrs[0], dir_entry_buf) == 0);
+  dir_entry_t *dir_entry = (dir_entry_t*)dir_entry_buf;
+  printf("Let's see what's inside the root directory:\n");
+  while (cur_ptr < 2) {
+    uint32_t name_length = dir_entry->lname_length | (dir_have_ti ? 0 : (dir_entry->hname_length << 8));
+    // if (!name_length)
+    printf("%d ", name_length);
+
+    for (int i = 0; i < name_length; i++) {
+      printf("%c", dir_entry->name[i]);
+    }
+    dir_entry += (8 + name_length) + 4 - (8 + name_length) % 4;
+    cur_ptr++;
+    printf("\n");
+  }
+  // for (int i = 0; i < sizeof(dir_entry_t); i++) {
+  //   printf("%p ", dir_entry_buf[i]);
+  // }
+
+  kfree(dir_entry_buf, get_block_size(sp));
+  kfree(inode_buf, get_block_size(sp));
+}
+
 
 void init_fs() {
   // superblock_t sp;
@@ -37,11 +99,17 @@ void init_fs() {
     printf("FILESYSTEM NOT OK :c %d\n", fs_ok_ret);
     return;
   }
+
+  inode_sz = get_inode_sz(sp);
+  dir_have_ti = sp->required_features & FEAT_REQ_DIR_HAS_TYPE ? 1 : 0;
+
   printf("Number of blocks: %d\n", sp->total_blocks);
   printf("Number of bytes per block: %d\n", get_block_size(sp));
   printf("Number of inodes: %d\n", sp->total_inodes);
+  printf("Number of inodes per bg: %d\n", sp->inodes_per_bg);
   printf("Superblock lives at block: %d\n", sp->superblock_block_num);
   printf("Number of block groups: %d\n", get_number_of_bgs(sp));
+  printf("Number of blocks per bgs: %d\n", sp->blocks_per_bg);
   printf("Version: %d.%d\n", sp->major_version, sp->min_version);
 
   uint32_t bgdt_size = sizeof(bg_desc_t)*get_number_of_bgs(sp);
@@ -50,4 +118,14 @@ void init_fs() {
   read(true, 2048, bgdt_size, bgdt);
   printf("%d free inodes in block group 0\n", bgdt[0].unallocated_inodes_in_group);
   printf("%d directories in block group 0\n", bgdt[0].dirs_in_group);
+  printf("inode table lives at %p\n", bgdt[0].inode_table_addr);
+
+  // uint16_t *inode_buf = kalloc(get_block_size(sp));
+  // KASSERT(inode_buf);
+  // printf("root inode block addr: %d\n", bgdt[0].inode_table_addr + 1);
+  // read_block(sp, 0, bgdt[0].inode_table_addr + 1, inode_buf);
+  // for (int i = inode_sz*1; i < sizeof(inode_t) + 0x80; i++)
+  //   printf("%p ", inode_buf[i]);
+
+  read_inode(sp, bgdt, 2); // 2 is the inode of the root directory
 }
--- fs/ext2.h
+++ fs/ext2.h
@@ -37,7 +37,25 @@ typedef struct superblock {
   uint32_t major_version;
   uint16_t uid_that_can_use_reserved_blocks;
   uint16_t gid_that_can_use_reserved_blocks;
-} superblock_t;
+  // extended fields available after version 1
+  uint32_t first_non_reserved_inode; // in versions <= 1 this is 11
+  uint16_t inode_sz; // in bytes, in versions <= 1 this is 128
+  uint16_t superblock_bg_backup;
+  uint32_t opt_features;
+  uint32_t required_features;
+  uint32_t required_write_features;
+  uint32_t fs_id[4];
+  uint32_t vol_name[4];
+  uint32_t path_last_mounted_to[16];
+  uint32_t comp_alg;
+  uint8_t blocks_to_preallocate_for_files;
+  uint8_t blocks_to_preallocate_for_dirs;
+  uint16_t unused;
+  uint32_t jounal_id[4];
+  uint32_t jounal_inode;
+  uint32_t journal_dev;
+  uint32_t orphan_inode_list_head;
+} __attribute__((aligned(1024))) superblock_t;
 
 typedef struct bg_desc {
   uint32_t block_usage_bitmap_ba;
@@ -58,7 +76,7 @@ typedef struct inode {
   uint32_t creation; // UNIX Timestamp
   uint32_t last_modification; // UNIX Timestamp
   uint32_t deletion_time; // UNIX Timestamp
-  uint16_t gui;
+  uint16_t gid;
   uint16_t hardlnk_cnt; // hardlinks to this inode
   uint32_t sec_cnt; // sectors used by this inode
   uint32_t flags;
@@ -72,6 +90,30 @@ typedef struct inode {
   uint32_t hsize; // only avl if version >= 1
   uint32_t frag_ba;
   uint32_t os_specific2[3];
-};
+} inode_t;
+
+typedef struct dir_entry {
+  uint32_t inode;
+  uint16_t entry_sz;
+  uint8_t lname_length; // least significant 8 bits of name length
+  uint8_t hname_length; // most significant 8 bits of name length, or Type Indicator if corresponding feature bit is set
+  char name[];
+} dir_entry_t;
+
+#define FEAT_OPT_PREALLOC_BLOCKS 0x1
+#define FEAT_OPT_AFS 0x2
+#define FEAT_OPT_JOURNAL 0x4
+#define FEAT_OPT_INODE_EXTATTRIBS 0x8
+#define FEAT_OPT_RESIZE 0x10
+#define FEAT_OPT_DIR_HASHINDEX 0x20
+
+#define FEAT_REQ_COMP 0x1
+#define FEAT_REQ_DIR_HAS_TYPE 0x2
+#define FEAT_REQ_NEEDS_JOURNAL_REPLAY 0x4
+#define FEAT_REQ_JOURNAL_DEV 0x8
+
+#define FEAT_RO_SPARSE 0x1
+#define FEAT_RO_SIZE64 0x2
+#define FEAT_RO_DIR_BINTREE 0x4
 
 #endif // !EXT2_H
