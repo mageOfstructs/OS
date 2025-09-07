ac3b177f12af0ec180011d23682b13e52d00e269
mageOfStructs <jason.puschnig@gmx.at>
2025-09-07 19:18:57 +0200

    memcpy: fix false positive of pointer overlap

--- mem.c
+++ mem.c
@@ -1,12 +1,13 @@
 #include "mem.h"
+#include "printf.h"
 
 int memcpy(const void *src, void *dst, size_t s) {
   if (src == dst)
     return -1;
   else if (src < dst && (char *)src + s > (char *)dst)
-    return -1;
-  else if ((char *)dst + s > (char *)src)
-    return -1;
+    return -2;
+  else if (dst < src && (char *)dst + s > (char *)src)
+    return -3;
   asm("mov ecx, %0\n\t"
       "mov esi, %1\n\t"
       "mov edi, %2\n\t"
