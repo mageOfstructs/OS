03ba9993f901c9578be26c4d492888ec4acaa927
mageOfStructs <jason.puschnig@gmx.at>
2025-09-08 16:02:20 +0200

    cleanup of kernel.c

--- kernel.c
+++ kernel.c
@@ -48,9 +48,7 @@ int main() {
 
   PIC_remap(0x20, 0x28);
   idt_init();
-  setup_vm(); // TODO: need to setup an address space for usermode
-
-  // jump_usermode();
+  setup_vm();
 
   uint16_t buf[256];
   int ret = identify(buf);
@@ -90,25 +88,5 @@ int main() {
     }
   }
 
-  // enable_cursor(0, 15);
-  // *((int *)0xb8000) = 0x07690748;
-  char *test = "Test";
-  int asdf = 43;
-  // printf("%c", *((char *)0x99999999));
-  // printf("%s %s %s", test, "asdf", "fdas");
-  printf("test\n");
-  // printf("test2\n");
-  // int tmp = 1 / 0;
-  // printf("test3\n");
-  // asm volatile("int 0x00");
-  // printf("test2");
-  // printf("%s %d", test, asdf);
-  // printf("%p", &asdf);
-  // write_str("Tessssssssssssssssssssssssssssssssssssssssssssssst",
-  //           (volatile u16 *)0xB8000);
-  // char *mem = (char *)0xb8000;
-  // for (int i = 0; i < 100; i++) {
-  //   *mem = 'Q';
-  //   mem += 2;
-  // }
+  printf("kernel end\n");
 }
