287d837268edf3bee6cd7e1f5104fffad0d1bf75
mageOfStructs <jason.puschnig@gmx.at>
2025-09-09 08:17:01 +0200

    add sys_write

--- syscall.h
+++ syscall.h
@@ -1,6 +1,12 @@
 #ifndef SYSCALL_H
 #define SYSCALL_H
 
+#include <stdint.h>
+
 void sys_test();
+void sys_write(uint32_t fd, void *buf, uint32_t sz);
+
+#define SYS_WRITE 1
+#define SYS_HLT 99
 
 #endif // !SYSCALL_H
