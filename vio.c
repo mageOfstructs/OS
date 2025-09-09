2b646891dee2c05ab18711932337d96708ee9235
mageOfStructs <jason.puschnig@gmx.at>
2025-09-09 08:18:01 +0200

    add open_vio

--- vio.c
+++ vio.c
@@ -1,4 +1,15 @@
 #include "vio.h"
+#include "fildes.h"
+#include <stdint.h>
+
+fildes_t open_vio(uint32_t stream, uint8_t perms) {
+  fildes_t ret = {.cursor = 0,
+                  .sz = -1,
+                  .type = VIRT_STREAM_TYPE,
+                  .perms = perms,
+                  .data = {.vio_data = {.vio_type = stream}}};
+  return ret;
+}
 
 int write_vio(fildes_t *fildes, uint32_t n, const void *src) {
   if (fildes->data.vio_data.vio_type == STDOUT) {
--- vio.h
+++ vio.h
@@ -10,5 +10,6 @@
 
 int write_vio(fildes_t *fildes, uint32_t n, const void *src);
 void close_vio(fildes_t *fildes);
+fildes_t open_vio(uint32_t stream, uint8_t perms);
 
 #endif // !VIO_H
