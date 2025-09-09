35f8f2c9ef0236bb408515f1f0211312b7b7e95d
mageOfStructs <jason.puschnig@gmx.at>
2025-09-09 08:18:53 +0200

    call some syscalls in test prog

--- test_usermode.c
+++ test_usermode.c
@@ -1,5 +1,16 @@
+#include <stdint.h>
 void test_user_function() {
+  const char *test = "Awawawawawawa\n";
+  const uint32_t test_sz = 15;
   asm("int 0x80");
-  for (;;)
-    ;
+  int asdf = 0;
+  asdf++;
+  asm("push %2\n\t"
+      "push %1\n\t"
+      "push %0 \n\t"
+      "mov eax, 1\n\t"
+      "int 0x80" ::"r"(1),
+      "m"(test), "r"(test_sz));
+  // asm("push eax; int 0x80");
+  asm("mov eax, 99; int 0x80");
 }
