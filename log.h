0cd636c6b0a02be59e1d26537534a839cafb01e4
mageOfStructs <jason.puschnig@gmx.at>
2025-09-09 08:49:24 +0200

    add logging helpers

---
+++ log.h
@@ -0,0 +1,19 @@
+#ifndef LOG_H
+#define LOG_H
+
+#include <stdarg.h>
+
+#define log(format, ...)                                                       \
+  printf("%s: " format, __func__ __VA_OPT__(, ) __VA_ARGS__);
+
+#define warn(format, ...)                                                      \
+  printf("[WARN file: " __FILE__                                               \
+         "; line: " CONST_TOSTR(__LINE__) "] %s: " format,                     \
+         __func__ __VA_OPT__(, ) __VA_ARGS__);
+
+#define err(format, ...)                                                       \
+  printf("\n[ERR file: " __FILE__                                              \
+         "; line: " CONST_TOSTR(__LINE__) "] %s: " format "\n",                \
+         __func__ __VA_OPT__(, ) __VA_ARGS__);
+
+#endif // !LOG_H
