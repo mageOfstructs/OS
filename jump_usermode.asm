fdff3984608b947386277ff46c735be1eaaf020d
mageOfStructs <jason.puschnig@gmx.at>
2025-09-10 17:18:52 +0200

    jump_usermode: don't modify edi

--- jump_usermode.asm
+++ jump_usermode.asm
@@ -1,7 +1,7 @@
 global jump_usermode
 
 jump_usermode:
-  mov edi, [esp+4] ; start address of usermode program
+  mov ebp, esp
 	mov ax, (4 * 8) | 3 ; ring 3 data with bottom 2 bits set for ring 3
 	mov ds, ax
 	mov es, ax 
@@ -9,10 +9,9 @@ jump_usermode:
 	mov gs, ax ; SS is handled by iret
 
 	; set up the stack frame iret expects
-	mov eax, [esp+8]
 	push (4 * 8) | 3 ; data selector
-	push eax ; current esp
+	push dword [ebp+8] ; user esp
 	pushf ; eflags
 	push (3 * 8) | 3 ; code selector (ring 3 code with bottom 2 bits set for ring 3)
-	push edi ; instruction address to return to
+	push dword [ebp+4] ; instruction address to return to
 	iret
