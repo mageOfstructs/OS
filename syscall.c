1fb086435897ba6f333ac95a0f60c3691d7291d7
mageOfStructs <jason.puschnig@gmx.at>
2025-09-13 13:28:23 +0200

    add sys_fork and sys_exit

--- syscall.c
+++ syscall.c
@@ -1,9 +1,12 @@
 #include "syscall.h"
 #include "fildes.h"
+#include "malloc.h"
 #include "printf.h"
 #include "fd_perms.h"
 #include "proc.h"
 #include "usermode.h"
+#include "vm.h"
+#include <stdint.h>
 
 void sys_write(uint32_t fd, void *buf, uint32_t sz) {
   write(&myproc()->fds[fd], sz, buf);
@@ -21,5 +24,55 @@ void sys_exec(char *path) {
     return;
   }
   load_usermode_prog(&new_prog_fd);
-  close_ext2(&new_prog_fd); // FIXME: this will never execute
+}
+
+// void sys_fork() {
+//   // we don't want to get interrupted by the timer while
+//   // PLIST is in an invalid state
+//   asm volatile("cli"); // maybe make this section smaller?
+//   proc_t *np = new_proc();
+//
+//   np->ctx.addr_sp_sz = myproc()->ctx.addr_sp_sz;
+//   uint32_t usercode_pages = ceild(np->ctx.addr_sp_sz, PG_SIZE);
+//   KASSERT(np->ctx.addr_sp =
+//               kalloc((np->ctx.addr_sp_sz + USER_STACK_PAGES) * 4));
+//   // TODO : we currently don't get any writable data segment as
+//   // load_usermode_prog is too naive for that
+//   KASSERT(vm_map_buf(np->ctx.addr_sp, usercode_pages, false, true) == 0);
+//   KASSERT(vm_map_buf(np->ctx.addr_sp + usercode_pages * 4, USER_STACK_PAGES,
+//                      true, true) == 0);
+//
+//   // setup the user stack
+//   np->ctx.regs.ebp = DEF_USERPROG_START + usercode_pages * PG_SIZE +
+//                      USER_STACK_PAGES * PG_SIZE - 1;
+//   np->ctx.regs.esp = np->ctx.regs.ebp;
+//   asm volatile("sti");
+// }
+
+void sys_fork(proc_ctx_t *ctx) {
+  asm volatile("cli"); // maybe make this section smaller?
+  proc_t *np = new_proc();
+
+  np->ctx.addr_sp_sz = myproc()->ctx.addr_sp_sz;
+  uint32_t usercode_pages = ceild(np->ctx.addr_sp_sz, PG_SIZE);
+  KASSERT(np->ctx.addr_sp =
+              kalloc((np->ctx.addr_sp_sz + USER_STACK_PAGES) * 4));
+  // TODO : we currently don't get any writable data segment as
+  // load_usermode_prog is too naive for that
+  KASSERT(vm_map_buf(np->ctx.addr_sp, usercode_pages, false, true) == 0);
+  KASSERT(vm_map_buf(np->ctx.addr_sp + usercode_pages * 4, USER_STACK_PAGES,
+                     true, true) == 0);
+
+  // setup the user stack
+  memcpy(ctx, &np->ctx, sizeof(proc_ctx_t) - 8);
+  asm volatile("sti");
+}
+
+// TODO: exit code
+void sys_exit() {
+  // asm volatile("cli");
+  myproc()->state = DEAD;
+  for (;;)
+    asm("hlt");
+  // asm volatile("sti");
 }
--- syscall.h
+++ syscall.h
@@ -1,6 +1,7 @@
 #ifndef SYSCALL_H
 #define SYSCALL_H
 
+#include "proc.h"
 #include <stdint.h>
 
 void sys_test();
@@ -8,9 +9,16 @@ void sys_write(uint32_t fd, void *buf, uint32_t sz);
 void sys_read(uint32_t fd, void *buf, uint32_t sz);
 void sys_exec(char *path);
 
+void sys_fork(proc_ctx_t *ctx);
+// void sys_fork();
+
+void sys_exit();
+
 #define SYS_WRITE 1
 #define SYS_READ 2
 #define SYS_EXEC 11
+#define SYS_FORK 12
 #define SYS_HLT 99
+#define SYS_EXIT 100
 
 #endif // !SYSCALL_H
