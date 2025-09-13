e82147925c05b8db46cf87dafd01264b2b0d7719
mageOfStructs <jason.puschnig@gmx.at>
2025-09-13 13:30:05 +0200

    test_usermode do sys_fork

--- test_usermode.c
+++ test_usermode.c
@@ -26,8 +26,23 @@ void test_user_function() {
       "mov eax, 1\n\t"
       "int 0x80\n\t" ::"r"(1),
       "r"(&buf), "r"(4));
-  asm("push %0\n\t"
-      "mov eax, 11\n\t"
-      "int 0x80" ::"m"(path));
+  asm("mov eax, 12\n\t"
+      "int 0x80" ::
+          : "eax");
+  asm("push %2\n\t"
+      "push %1\n\t"
+      "push %0 \n\t"
+      "mov eax, 1\n\t"
+      "int 0x80\n\t" ::"r"(1),
+      "m"(test), "r"(test_sz)
+      : "eax");
+  asm("mov eax, 100\n\t"
+      "int 0x80" ::
+          : "eax");
+  // sys_exec:
+  // asm("push %0\n\t"
+  //     "mov eax, 11\n\t"
+  //     "int 0x80" ::"m"(path));
+  // sys_hlt:
   // asm("mov eax, 99; int 0x80");
 }
