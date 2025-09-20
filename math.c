956cf1fcc1d211c034392c53de3de7652bcea911
mageOfStructs <jason.puschnig@gmx.at>
2025-09-20 11:12:28 +0200

    add pad to math lib

--- math.c
+++ math.c
@@ -1,13 +1,14 @@
 #include "math.h"
+#include <stdint.h>
 
 inline uint32_t ceild(uint32_t divid, uint32_t divis) {
   return divid / divis + (divid % divis != 0 ? 1 : 0);
 }
 
-inline uint32_t max(uint32_t n1, uint32_t n2) {
-  return n1 > n2 ? n1 : n2;
-}
+inline uint32_t max(uint32_t n1, uint32_t n2) { return n1 > n2 ? n1 : n2; }
 
-inline uint32_t min(uint32_t n1, uint32_t n2) {
-  return n1 < n2 ? n1 : n2;
+inline uint32_t min(uint32_t n1, uint32_t n2) { return n1 < n2 ? n1 : n2; }
+
+inline uint32_t pad(uint32_t n, uint32_t div) {
+  return n + (div - n % div) % div;
 }
--- math.h
+++ math.h
@@ -4,4 +4,5 @@
 uint32_t ceild(uint32_t divid, uint32_t divis);
 uint32_t max(uint32_t n1, uint32_t n2);
 uint32_t min(uint32_t n1, uint32_t n2);
+uint32_t pad(uint32_t n, uint32_t div);
 #endif // !MATH_H
